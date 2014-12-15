fd7ff42892a6774dcc97145b10f1798bb0453e83
marthaberner <marthaberner.mb@gmail.com>
2014-12-14 17:29:14 -0800

    user can crud a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path, notice: "Task was successfully updated"
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, notice: "Task was successfully deleted"
+  end
+
   private
 
   def task_params
